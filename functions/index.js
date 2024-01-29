258d604c5c2c89690d7625fc83d1f2237b63a159
Ayush shukla <ayushshukla3999@gmail.com>
2024-01-30 01:43:04 +0530

    second

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,28 @@
 // index.js
 const express = require('express');
+const serverless = require("serverless-http");
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const pino = require('express-pino-logger')();
 const client = require('twilio')(
   "ACdf657a64445f670c3d884ed4aa0ea564",
-  'c0c278160948485ae0862dba36af86ea'
+  '325d4643a2804212f68682a00eb5dbb0'
 );
 
 const app = express();
+const router = express.Router();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(pino); 
 
-app.get('/api/messages', (req, res) => {
+router.get('/', (req, res) => {
+  res.send('App is running..');
+});
+
+router.get('/api/messages', (req, res) => {
   res.header('Content-Type', 'application/json');
   client.messages
     .create({
@@ -33,7 +39,7 @@ app.get('/api/messages', (req, res) => {
     });
 });
 
+app.use(`/.netlify/functions/index`, router);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
